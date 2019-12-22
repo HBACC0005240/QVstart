815708cdd03cb485a3b0181ba4fa626593955b51
wangqi <hbacc0005240@163.com>
2019-12-22 22:53:17 +0800

    update

--- Common/ITApplication.cpp
+++ Common/ITApplication.cpp
@@ -1,6 +1,8 @@
 #include "ITApplication.h"
 #include <QWidget>
+#include <QFileInfo>
 #include <QDesktopServices>
+#define TIME_OUT 500  /// (ms)
 
 #define Drag_Epsilon	3
 
@@ -18,12 +20,18 @@ enum DragDirection
 };
 
 ITApplication::ITApplication(int &argc, char **argv, int _internal)
-	: QApplication(argc,argv,_internal)
+	: QApplication(argc,argv,_internal), m_pMainWindow(NULL), m_bIsRunning(false), m_pLocalServer(NULL)
 	, m_uiDragSizeMode(Dir_None)
 	, m_bStartDragSize(false)
 {
+	/// 取应用程序名作为LocalServer的名字
+	m_strServerName = QFileInfo(QCoreApplication::applicationFilePath()).fileName();
+	initLocalConnection();
 }
-
+void ITApplication::setWindowInstance(QWidget *wnd)
+{
+	m_pMainWindow = wnd;
+}
 unsigned int ITApplication::getDragDirection()
 {
 	QWidget* activeWidget = qApp->activeWindow();
@@ -168,11 +176,109 @@ bool ITApplication::notify(QObject *pObj, QEvent *ev)
 				m_uiDragSizeMode = Dir_None;
 				restoreOverrideCursor();
 			}			
-			break;
+			break;	
 		default:
 			break;
 		}
 	}	
 	bool result = QApplication::notify(pObj, ev);
 	return result;
+}
+
+//************************************
+//函数名:  isRunning
+//描述：检查是否有一个实例运行
+//参数：无
+//返回值：bool
+//时间：2016/11/9 WZQ
+//************************************
+bool ITApplication::isRunning()
+{
+	return m_bIsRunning;
+}
+//************************************
+//函数名:  newLocalConnection
+//描述：通过socket通讯实现程序单例运行，监听到新的连接时触发
+//参数：无
+//返回值：无
+//时间：2016/11/9 WZQ
+//************************************
+void ITApplication::newLocalConnection()
+{
+	QLocalSocket* pSocket = m_pLocalServer->nextPendingConnection();
+	if (pSocket != NULL)
+	{
+		pSocket->waitForReadyRead(2 * TIME_OUT);
+		delete pSocket;
+		activateWindow();
+	}/// end if(pSocket != NULL)
+}
+//************************************
+//函数名:  initLocalConnection
+//描述：初始化本地连接，如果连接不上server，则创建，否则退出
+//参数：无
+//返回值：void
+//时间：2016/11/9 WZQ
+//************************************
+void ITApplication::initLocalConnection()
+{
+	m_bIsRunning = false;
+	QLocalSocket socket;
+	socket.connectToServer(m_strServerName);
+	if (socket.waitForConnected(TIME_OUT))
+	{
+		fprintf(stderr, "%s already running.\n", m_strServerName.toLocal8Bit().constData());
+		m_bIsRunning = true;
+		return;
+	}/// end if(socket.waitForConnected(TIME_OUT))
+	/// 如果连接不上服务器，就创建一个
+	newLocalServer();
+}
+//************************************
+//函数名:  newLocalServer
+//描述：LocalServer
+//参数：无
+//返回值：void
+//时间：2016/11/9 WZQ
+//************************************
+void ITApplication::newLocalServer()
+{
+	m_pLocalServer = new QLocalServer(this);
+	connect(m_pLocalServer, SIGNAL(newConnection()), this, SLOT(newLocalConnection()));
+	if (!m_pLocalServer->listen(m_strServerName))
+	{
+		/// 此时监听失败，可能是程序崩溃时，残留进程服务导致的，将其移除之
+		if (m_pLocalServer->serverError() == QAbstractSocket::AddressInUseError)
+		{
+			QLocalServer::removeServer(m_strServerName);
+			m_pLocalServer->listen(m_strServerName);
+		}/// end fi(m_pLocalServer->serverError() == QAbstractSocket::AddressInUseError)
+	}/// end if(!m_pLocalServer->listen(m_strServerName))
+}
+//************************************
+//函数名:  activateWindow
+//描述：激活主窗口
+//参数：无
+//返回值：void
+//时间：2016/11/9 WZQ
+//************************************
+void ITApplication::activateWindow()
+{
+	if (m_pMainWindow != NULL)
+	{
+		m_pMainWindow->activateWindow();
+		m_pMainWindow->showNormal();
+		m_pMainWindow->raise();
+	}
+}
+//************************************
+//函数名:  disLocalConnect
+//描述：断开本地服务器信号连接
+//参数：无
+//返回值：无
+//时间：2016/11/9 WZQ
+//************************************
+void ITApplication::disLocalConnect()
+{
+	disconnect(m_pLocalServer, SIGNAL(newConnection()), this, SLOT(newLocalConnection()));
 }
--- Common/ITApplication.h
+++ Common/ITApplication.h
@@ -3,6 +3,9 @@
 
 #include <QApplication>
 #include <QKeyEvent>
+#include <QtNetwork/QLocalServer>
+#include <QWidget>
+#include <QtNetwork/QLocalSocket>
 
 class ITApplication : public QApplication
 {
@@ -13,8 +16,20 @@ public:
 	~ITApplication(){}
 
 	bool notify(QObject *pObj, QEvent *ev);
-
+	void setWindowInstance(QWidget* wnd);
+	/// 是否已经有实例运行
+	bool isRunning();
+	void disLocalConnect();	//断开本进程的连接
+public slots:
+	/// 有链接时触发
+	void newLocalConnection();
 private:
+	/// 初始化本地连接
+	void initLocalConnection();
+	/// 创建服务端
+	void newLocalServer();
+	/// 激活窗口
+	void activateWindow();
 	unsigned int getDragDirection();
 
 private:
@@ -25,6 +40,10 @@ private:
 	int						m_iOriginHeight;
 	unsigned int			m_uiDragSizeMode;
 	bool					m_bStartDragSize;
+	bool m_bIsRunning;				/// 是否已经有实例运行
+	QLocalServer* m_pLocalServer;	/// 本地Socket Server
+	QString m_strServerName;		///服务名称 
+	QWidget* m_pMainWindow;//保存一个你窗体的指针
 };
 
 #endif //ITAPPLICATION_H
