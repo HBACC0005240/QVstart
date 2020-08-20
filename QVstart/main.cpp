736f5c77baa2fd1ab77c4d9aef923bde03ec0bd6
wzq <wangzq-c@glodon.com>
2020-08-20 11:31:03 +0800

    提交

--- QVstart/main.cpp
+++ QVstart/main.cpp
@@ -7,7 +7,7 @@
 #include "ITCollator.h"
 #include "PublicFun.h"
 using namespace std;
-
+#include "YunLai.h"
 int main(int argc, char *argv[])
 {	
 	//	QApplication a(argc, argv);
@@ -23,29 +23,41 @@ int main(int argc, char *argv[])
 	translator.load(QString(":/QVStart/Resources/qt_zh_CN.qm"));
 	a.installTranslator(&translator);
 
-	QLocale local(QLocale::English);
-	ITCollator collator(local);
-	QStringList stringList;
-	stringList << QString::fromLocal8Bit("天地图")
-		<< QString::fromLocal8Bit("微软")
-		<< QString::fromLocal8Bit("456哈哈")
-		<< QString::fromLocal8Bit("123微软")
-		<< QString::fromLocal8Bit("a气象")
-		<< QString::fromLocal8Bit("b中国")
-		<< QString::fromLocal8Bit("猪猪")
-		<< QString::fromLocal8Bit("地质图")
-		<< QString::fromLocal8Bit("KL1")
-		<< QString::fromLocal8Bit("KL10")
-		<< QString::fromLocal8Bit("KL2")
-		<< QString::fromLocal8Bit("KL3")
-		<< QString::fromLocal8Bit("KL13")
-		<< QString::fromLocal8Bit("KL1(2)")
-		<< QString::fromLocal8Bit("KL1(1)");
+	//QLocale local(QLocale::English);
+	//ITCollator collator(local);
+	//QStringList stringList;
+	//stringList << QString::fromLocal8Bit("天地图")
+	//	<< QString::fromLocal8Bit("微软")
+	//	<< QString::fromLocal8Bit("456哈哈")
+	//	<< QString::fromLocal8Bit("123微软")
+	//	<< QString::fromLocal8Bit("a气象")
+	//	<< QString::fromLocal8Bit("b中国")
+	//	<< QString::fromLocal8Bit("猪猪")
+	//	<< QString::fromLocal8Bit("地质图")
+	//	<< QString::fromLocal8Bit("KL1")
+	//	<< QString::fromLocal8Bit("KL10")
+	//	<< QString::fromLocal8Bit("KL2")
+	//	<< QString::fromLocal8Bit("KL3")
+	//	<< QString::fromLocal8Bit("KL13")
+	//	<< QString::fromLocal8Bit("KL1(2)")
+	//	<< QString::fromLocal8Bit("KL1(1)");
 
-	//qSort(stringList.begin(), stringList.end(), collator);//不行 qt内部最后调用的还是Windows自身API CompareString 现在它排序不符合要求
-	qSort(stringList.begin(), stringList.end(), customCompareString);
+	////qSort(stringList.begin(), stringList.end(), collator);//不行 qt内部最后调用的还是Windows自身API CompareString 现在它排序不符合要求
+	//qSort(stringList.begin(), stringList.end(), customCompareString);
 
-
+	QMap<qint64, QString> process;
+	YunLai::GetAllProcess(process);
+	for (auto it=process.begin();it!=process.end();++it)
+	{
+		QString szProcessText = it.value();
+		if ("notepad++.exe" == szProcessText)
+		{
+			HWND pHwnd = YunLai::FindMainWindow(it.key());
+			YunLai::SetWindowHide(pHwnd);
+			HBITMAP hBmp = YunLai::CopyScreenToBitmap(pHwnd);
+			YunLai::SaveBitmapToFile(hBmp, QString("%1.bmp").arg(QDateTime::currentMSecsSinceEpoch()).toStdString());
+		}
+	}
 	QVStart w;
 	//	w.resize(100,300);
 	QRect deskRect = QApplication::desktop()->availableGeometry();
